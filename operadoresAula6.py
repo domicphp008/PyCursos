40b4dd70f04fe31ed22256d6369893e51fe1b2f7
Domicio Jose <testedomicio@hotmail.com>
2016-01-06 10:52:18 -0200

    operadores
    Aula 06

---
+++ operadoresAula6.py
@@ -0,0 +1,9 @@
+Python 3.4.4 (v3.4.4:737efcadf5a6, Dec 20 2015, 20:20:57) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 2/4
+0.5
+>>> 2 + 3j
+(2+3j)
+>>> 2 +3j *4j
+(-10+0j)
+>>> 
