4a1805f4cece643cd962dd9a601d203533e9b3cb
Domicio Jose <testedomicio@hotmail.com>
2016-01-06 00:02:17 -0200

    aula 2
    Loops (Laços) While com Python

---
+++ wihile-shell.py
@@ -0,0 +1,32 @@
+Python 3.4.4 (v3.4.4:737efcadf5a6, Dec 20 2015, 20:20:57) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+==================== RESTART: D:/GitHub/PyCursos/while.py ====================
+0
+1
+3
+6
+10
+15
+21
+28
+36
+45
+>>> 
+==================== RESTART: D:/GitHub/PyCursos/while.py ====================
+0
+1
+3
+6
+10
+15
+21
+28
+36
+45
+valor final 45
+>>> 
+==================== RESTART: D:/GitHub/PyCursos/while.py ====================
+0
+valor final 0
+>>> 
