517b83dd8e8fdabb63abe02300ab8c3596402b3a
Domicio Jose <testedomicio@hotmail.com>
2016-01-06 00:18:10 -0200

    concatenação de Strings e Inteiros
    Aula 4

---
+++ concatenação-deStr-int.py
@@ -0,0 +1,21 @@
+Python 3.4.4 (v3.4.4:737efcadf5a6, Dec 20 2015, 20:20:57) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 'hello'
+'hello'
+>>> x = 'hello'
+>>> y = 42
+>>> z = 'there'
+>>> 
+>>> print(x + z)
+hellothere
+>>> print(x + '' + z)
+hellothere
+>>> print(x,y)
+hello 42
+>>> print(x,z)
+hello there
+>>> print(x+str(y))
+hello42
+>>> print(x,y,z)
+hello 42 there
+>>> 
