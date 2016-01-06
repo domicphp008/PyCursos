6c860dda940aba86049dfe575c54b84e80fff479
Domicio Jose <testedomicio@hotmail.com>
2016-01-05 23:51:46 -0200

    aula1 loop-for

---
+++ loop-for.py
@@ -0,0 +1,7 @@
+x = ['poderoso','simples','a melhor linguagem']
+for palavra in x:
+    print('Ptyhon é' , palavra)
+    
+for i in range (len(x)):
+    print('Python é', x[i])
+    
---
+++ loop.py
@@ -0,0 +1,20 @@
+Python 3.4.4 (v3.4.4:737efcadf5a6, Dec 20 2015, 20:20:57) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================== RESTART: D:/GitHub/PyCursos/loop-for.py ==================
+Ptyhon é poderoso
+Ptyhon é simples
+Ptyhon é a melhor linguagem
+>>> range(4)
+range(0, 4)
+>>> list(range(4))
+[0, 1, 2, 3]
+>>> 
+================== RESTART: D:/GitHub/PyCursos/loop-for.py ==================
+Ptyhon é poderoso
+Ptyhon é simples
+Ptyhon é a melhor linguagem
+Python é poderoso
+Python é simples
+Python é a melhor linguagem
+>>> 
