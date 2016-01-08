ae690fb6e6b4f5ac4d13804a15e99d122e6dfdee
Domicio Jose <testedomicio@hotmail.com>
2016-01-08 10:19:32 -0200

    Aula 08a

---
+++ Entrada-de-DadosAula08.py
@@ -0,0 +1,36 @@
+Python 3.4.4 (v3.4.4:737efcadf5a6, Dec 20 2015, 20:20:57) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> dívida = 0
+>>> compra = 42
+>>> dívida = dívida + compra
+>>> compra = 13
+>>> dívida = dívida + compra
+>>> compra = 50
+>>> dívida = dívida + compra
+>>> print (dívida)
+105
+>>> 
+====================== RESTART: C:/Python34/zumbi20.py ======================
+Domicio
+>>> 
+====================== RESTART: C:/Python34/zumbi20.py ======================
+Elda
+>>> 
+====================== RESTART: C:/Python34/zumbi20.py ======================
+Digite o seu nome: Domicio
+Domicio
+>>> 
+====================== RESTART: C:/Python34/zumbi20.py ======================
+Digite o primeiro número: 42
+Digite o segundo número: 13
+4213
+>>> "42"
+'42'
+>>> "13"
+'13'
+>>> 
+====================== RESTART: C:/Python34/zumbi20.py ======================
+Digite o primeiro número: 42
+Digite o segundo número: 13
+55
+>>> 
