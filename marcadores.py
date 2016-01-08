869d98c6caf205727213952caa470dfff88f14a8
Domicio Jose <testedomicio@hotmail.com>
2016-01-07 23:23:33 -0200

    Aulas 5, 6 e uma repetida
    python para Zumbis

---
+++ marcadores.py
@@ -0,0 +1,61 @@
+Python 3.4.4 (v3.4.4:737efcadf5a6, Dec 20 2015, 20:20:57) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> a = 42
+>>> b = 13
+>>> a > b
+True
+>>> type(True)
+<class 'bool'>
+>>> type(False)
+<class 'bool'>
+>>> a < b
+False
+>>> a == 42
+True
+>>> b == 42
+False
+>>> b
+13
+>>> b = 42
+>>> b
+42
+>>> a!= 13
+True
+>>> a
+42
+>>> nota = 7
+>>> faltas = 30
+>>> nota >= 6 and faltas <= 20
+False
+>>> a
+42
+>>> b
+42
+>>> a == 42 or b == 42
+True
+>>> a = 13
+>>> a == 42 or b == 42
+True
+>>> b =13
+>>> a == 42 or b == 42
+False
+>>> print ( ' O número é  42 )
+	
+SyntaxError: EOL while scanning string literal
+>>> print ( ' O número é  42 ')
+ O número é  42 
+>>> # Obs: um erro acima e foi corrigido !
+>>> print (' O número' , a, 'é muito legal')
+ O número 13 é muito legal
+>>> # Usando marcador
+>>> print (' O número %d é muito legal' %a)
+ O número 13 é muito legal
+>>> a = 'abacate'
+>>> print ('Uma fruta muito gostosa é o %s. Na opinião do Massa' %a)
+Uma fruta muito gostosa é o abacate. Na opinião do Massa
+>>> a = 3.1415
+>>> print ('Pi é %f' %a)
+Pi é 3.141500
+>>> print ('Pi com duas casas %.2f' %a)
+Pi com duas casas 3.14
+>>> 
---
+++ operadores-Aula repedita.py
@@ -0,0 +1,53 @@
+Python 3.4.4 (v3.4.4:737efcadf5a6, Dec 20 2015, 20:20:57) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> a = 42
+>>> b = 13
+>>> a > b
+True
+>>> type(True)
+<class 'bool'>
+>>> a < b
+False
+>>> a == 42
+True
+>>> b == 42
+False
+>>> b = 42
+>>> b
+42
+>>> a != 13
+True
+>>> a
+42
+>>> nota = 7
+>>> faltas = 30
+>>> nota >= 6 and flatas <= 20
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    nota >= 6 and flatas <= 20
+NameError: name 'flatas' is not defined
+>>> nota >= 6 and faltas <= 20
+False
+>>> a
+42
+>>> b
+42
+>>> a ==42
+True
+>>> a == 42 or b == 42
+True
+>>> a = 13
+>>> a == 42 or b ==42
+True
+>>> b = 1'3
+SyntaxError: EOL while scanning string literal
+>>> b = 13
+>>>  a == 42 or b == 42
+ 
+SyntaxError: unexpected indent
+>>> b = 13
+>>> a == 42 or b == 42
+False
+>>> #voltar o que foi digitado clique em Alt + p para frente
+>>> # Alt + n para trás
+>>> # Nesta aula houve três erros de digitação e espaçamentos.
