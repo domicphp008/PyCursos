e823308305fc3245e6e2b458cbccf6c53ffcfaa6
Domicio Jose <testedomicio@hotmail.com>
2016-01-10 13:22:39 -0200

    Fatiamento
    Fastiamento do primeiro índice até o anterior do segundo

---
+++ FatiamentoAula31.py
@@ -0,0 +1,14 @@
+Python 3.4.4 (v3.4.4:737efcadf5a6, Dec 20 2015, 20:20:57) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> x = "0123456789"
+>>> print(x[0:2])
+01
+>>> print(x[1:2])
+1
+>>> print(x[2:4])
+23
+>>> print(x[0:5])
+01234
+>>> print(x[1:8])
+1234567
+>>> 
