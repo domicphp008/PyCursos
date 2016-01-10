e91859c297eb6d550f4b1aaf2670dd5318f0aa01
Domicio Jose <testedomicio@hotmail.com>
2016-01-10 10:48:42 -0200

    Vetor

---
+++ VetorAula27.py
@@ -0,0 +1,17 @@
+'''
+          Listas
+    Prof: Fernando masanori
+     e-mail: fmasanori@gmail.com
+    Aluno: Domício
+     e-mail:domiciopro@gmail.com
+     . Vetor
+     
+'''
+vetor = []
+i = 1
+while i <= 10:
+    n = float(input("Digite um número: "))
+    vetor.append(n)
+    i = i + 1
+    i = 9
+print ("Vetor lido: ", vetor)    
---
+++ VetorInversaAula27.py
@@ -0,0 +1,19 @@
+'''
+          Listas
+    Prof: Fernando masanori
+     e-mail: fmasanori@gmail.com
+    Aluno: Domício
+     e-mail:domiciopro@gmail.com
+     . Vetor
+     
+'''
+vetor = []
+i = 1
+while i <= 10:
+    n = float(input("Digite um número: "))
+    vetor.append(n)
+    i = i + 1
+i = 9
+while i >= 0:
+    print (vetor[i])    
+    i -= 1 
