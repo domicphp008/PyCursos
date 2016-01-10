ad88352e2bfcfca42ec5a2f0c0ad1eae85b579b7
Domicio Jose <testedomicio@hotmail.com>
2016-01-10 13:17:12 -0200

    Aspas de vários tipos

---
+++ StringsAspasAula31.py
@@ -0,0 +1,31 @@
+Python 3.4.4 (v3.4.4:737efcadf5a6, Dec 20 2015, 20:20:57) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+=========== RESTART: D:/GitHub/PyCursos/ContarConsoantesAula30.py ===========
+Letra: a
+Letra: c
+Letra: e
+Letra: i
+Letra: s
+Letra: e
+Letra: t
+Letra: r
+Letra: o
+Letra: u
+Foram lidos 4 consoantes
+>>> x = 'abacate'
+>>> x
+'abacate'
+>>> y = "MacDonald's"
+>>> y
+"MacDonald's"
+>>> form = '''
+<html>
+	<head>
+	      <title> Teste </title>
+	      </head>
+	      <body>
+	            <p>Testando</p>
+	            </body>
+</html>
+
