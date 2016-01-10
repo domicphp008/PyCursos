f26b96b23597ae6a15cc445ede5a71035634f185
Domicio Jose <testedomicio@hotmail.com>
2016-01-09 23:05:26 -0200

    Tabuada

---
+++ RepetiçãoTabuadaAula22.py
@@ -0,0 +1,18 @@
+'''
+    Repetições anonhadas
+
+    Imprima as tabuadas de 1 a 10
+
+    Aluno: Domício
+
+'''
+#Abaixo vai trocando os números [ tabuada = 2] assim por diante.
+tabuada = 1
+while tabuada <= 10:
+    n = 1
+    print ("Tabuada %d" %tabuada)
+    while n <= 10:
+        print ("%d x %d = %d"
+               %(tabuada, n, tabuada * n))
+        n = n + 1
+        tabuada = tabuada + 1
